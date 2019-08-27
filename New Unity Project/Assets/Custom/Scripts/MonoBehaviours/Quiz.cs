0a4fabc7316290cc1d0c954d5cecd38f6fc93f96
sdad <asda>
2019-08-27 17:52:29 +1200

    fixed quiz bug and added a third question

--- New Unity Project/Assets/Custom/Scripts/MonoBehaviours/Quiz.cs
+++ New Unity Project/Assets/Custom/Scripts/MonoBehaviours/Quiz.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour {
     [Serializable]
     public class Question {
-        public string text;
-        public string asnwer;
+        public string sQuestion;
+        public string[] answers;
     }
 
     public Question[] questions;
@@ -14,17 +16,18 @@ public class Quiz : MonoBehaviour {
     public RectTransform pnlQuestion;
     public FinishScreen finishScreen;
     public Button btnStart;
-    public Button btnGood;
-    public Button btnBad;
+    public Button[] answerButtons;
     public Text txtQuestion;
 
     private int progress;
     private int answerCount = 0;
+    private void Update() {
+        Debug.Log(answerCount);
+    }
 
     private void Awake() {
         StaticMethods.AssignButtonAction(btnStart, AskQuestion);
-        StaticMethods.AssignButtonAction(btnGood, Answer);
-        StaticMethods.AssignButtonAction(btnBad, IncorrectAnswer);
+        answerButtons = pnlQuestion.GetComponentsInChildren<Button>(true);
     }
 
     private void ShowIntroduction() {
@@ -38,9 +41,8 @@ public class Quiz : MonoBehaviour {
     }
 
     private void Answer() {
-     //if i click the good button increment the answercount... just do not know how to acess the good button...
-        AskQuestion();
         answerCount++;
+        AskQuestion();
     }
 
     private void IncorrectAnswer(){
@@ -50,19 +52,46 @@ public class Quiz : MonoBehaviour {
     private void AskQuestion() {
         ShowQuestion();
 
-        if (progress >= questions.Length) {
+        if (progress == questions.Length) {
             EndQuiz();
         } else {
-            txtQuestion.text = questions[progress++].text;
+            txtQuestion.text = questions[progress].sQuestion;
+
+            // Shuffle answers.
+            List<int> ints = new List<int> { 0, 1, 2 };
+            StaticMethods.ShuffleList(ints);
+
+            AssignAnswer(0, ints[0]);
+            AssignAnswer(1, ints[1]);
+            AssignAnswer(2, ints[2]);
+
+            DebugAnswers(ints);
+
+            progress++;
         }
+    }
+
+    private void DebugAnswers(List<int> ints) {
+        for (int i = 0; i < answerButtons.Length; i++) {
+            answerButtons[i].GetComponent<Image>().color = Color.grey;
+
+            if (ints[i] == 0) {
+                answerButtons[i].GetComponent<Image>().color = Color.green;
+            }
+        }
+    }
+
+    private void AssignAnswer(int buttonIndex, int _i) {
+        StaticMethods.AssignButtonAction(answerButtons[buttonIndex], (_i == 0) ? (UnityAction)Answer : IncorrectAnswer);
+        answerButtons[buttonIndex].transform.GetChild(0).GetComponent<Text>().text = questions[progress].answers[_i];
     }
 
     private void EndQuiz() {
         gameObject.SetActive(false);
         finishScreen.gameObject.SetActive(true);
            Text _finishText = finishScreen.GetComponentInChildren<Text>();
-        Debug.Log(questions.Length);
-        if(answerCount == 2){
+     
+        if(answerCount == questions.Length){
            _finishText.text = "win";
 
         } else{
--- New Unity Project/Assets/Custom/Scripts/Static/StaticMethods.cs
+++ New Unity Project/Assets/Custom/Scripts/Static/StaticMethods.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine.Events;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public static class StaticMethods {
@@ -7,8 +9,15 @@ public static class StaticMethods {
 
         _button.onClick.AddListener(() => {
             _method();
-          
         });
     }
    
+    public static void ShuffleList<T>(List<T> list) {
+        for (int i = 0; i < list.Count; i++) {
+            T temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
 }
